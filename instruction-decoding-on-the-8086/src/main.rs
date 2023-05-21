334ac0ed07e2a7a30b1c9bf3b31bdc037b231f45
Atsushi Toda <todaatsushi0@gmail.com>
2023-05-21 18:39:39 +0100

    Read file and print out bytes

--- instruction-decoding-on-the-8086/src/main.rs
+++ instruction-decoding-on-the-8086/src/main.rs
@@ -1,3 +1,20 @@
+use std::io::Read;
+use std::{env, fs};
+
+fn read_binary() -> Vec<u8> {
+    let args: Vec<String> = env::args().collect();
+    let file: &str = args[1].as_str();
+
+    let mut input = fs::File::open(file).expect("Couldn't open file");
+    let mut buffer = Vec::new();
+
+    let _ = input.read_to_end(&mut buffer);
+    buffer
+}
+
 fn main() {
-    println!("Hello, world!");
+    let contents = read_binary();
+    for v in contents {
+        println!("{:08b}", v);
+    }
 }
